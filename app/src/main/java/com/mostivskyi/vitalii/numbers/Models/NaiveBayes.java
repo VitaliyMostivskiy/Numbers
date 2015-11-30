01a5756d8eec30974b07f5a15aa65f6713a747a8
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-12-01 00:14:29 +0100

    Refactor Naive Bayes

--- app/src/main/java/com/mostivskyi/vitalii/numbers/Models/NaiveBayes.java
+++ app/src/main/java/com/mostivskyi/vitalii/numbers/Models/NaiveBayes.java
@@ -9,58 +9,76 @@ public class NaiveBayes {
 
     private double[][] averages;
     private double[][] variances;
+    //вірогідність апріорі - відношення к-сті елементів даного класу до загю к-сті ел.
     private double[] classDistribution;
-    private List<Double> classes;
-    private boolean isTrained;
-    private double varianceNoice;
 
-    public NaiveBayes() {
-        isTrained = false;
-        varianceNoice = 0.00001;
+    private List<Double> classes;
+
+    private boolean isTrained;
+    private double varianceNoise;
+
+    public boolean isTrained() {
+        return isTrained;
     }
 
-    public void buildClassifier(DataCollection data) {
-        averages = new double[data.getClassesCount()][data.getFeaturesCount()];
-        variances = new double[data.getClassesCount()][data.getFeaturesCount()];
-        classDistribution = new double[data.getClassesCount()];
+    public NaiveBayes()
+    {
+        isTrained = false;
+        varianceNoise = 0.00001;
+    }
+
+    public void buildClassifier(DataCollection data)
+    {
+        int classesCount = data.getClassesCount();
+        int featuresCount = data.getFeaturesCount();
+        int recordsCount = data.getRecordsCount();
+
+        averages = new double[classesCount][featuresCount];
+        variances = new double[classesCount][featuresCount];
+        classDistribution = new double[classesCount];
+
         classes = data.getClasses();
 
-        for (int i = 0; i < data.getClassesCount(); i++) {
-            classDistribution[i] = ((double) data.getRecordsCountInClass(i)) / ((double) data.getRecordsCount());
+        for (int i = 0; i < classesCount; i++)
+        {
+            classDistribution[i] = ((double) data.getRecordsCountInClass(i)) / ((double) recordsCount);
 
-            for (int j = 0; j < data.getFeaturesCount(); j++) {
+            for (int j = 0; j < featuresCount; j++)
+            {
                 averages[i][j] = data.getAverageValue(j, i);
-                variances[i][j] = data.getVariation(j, i) + varianceNoice;
+                variances[i][j] = data.getVariation(j, i) + varianceNoise;
             }
         }
 
         isTrained = true;
     }
 
-    public double classifyInstance(Features features) {
-        double currenProbability = 0;
-        int currentId = 0;
+    public double classifyInstance(Features features)
+    {
         List<Double> featureValues = features.calculateFeatures();
+        int featuresCount = featureValues.size();
+        int classesCount = classes.size();
 
-        for (int i = 0; i < classes.size(); i++) {
+        double maxProbability = 0;
+        int maxClassId = 0;
+
+        for (int i = 0; i < classesCount; i++)
+        {
             double py = classDistribution[i];
 
-            for (int j = 0; j < featureValues.size(); j++) {
+            for (int j = 0; j < featuresCount; j++)
+            {
                 py = py * (1 / (Math.sqrt(2 * Math.PI * variances[i][j])))
                         * Math.exp(-1 / (2 * variances[i][j]) * Math.pow((featureValues.get(j) - averages[i][j]), 2));
             }
 
-            if (py > currenProbability)
+            if (py > maxProbability)
             {
-                currenProbability = py;
-                currentId = i;
+                maxProbability = py;
+                maxClassId = i;
             }
         }
 
-        return classes.get(currentId);
-    }
-
-    public boolean isTrained() {
-        return isTrained;
+        return classes.get(maxClassId);
     }
 }
